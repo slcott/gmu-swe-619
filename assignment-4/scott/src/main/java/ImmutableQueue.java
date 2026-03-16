9dc2ccb4401421b7a4ca2a0c48cbd78494e6f87a
Scott <s.riccardelli@gmail.com>
2026-03-15 20:13:31 -0400

    move work in assignment 4 to scott folder

---
+++ assignment-4/scott/src/main/java/ImmutableQueue.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ImmutableQueue<E> {
+   private final List<E> elements;
+   private final int size;
+
+   public ImmutableQueue() {   
+      this.elements = List.of();
+      this.size = 0;
+   }
+
+   private ImmutableQueue(List<E> elements) {
+      this.elements = List.copyOf(elements);
+      this.size = elements.size();
+   }
+
+   public ImmutableQueue<E> enQueue(E e) {
+      if (e == null) throw new NullPointerException("ImmutableQueue.enQueue");
+      List<E> newList = new ArrayList<>(this.elements);
+      newList.add(e);
+      return new ImmutableQueue<E>(newList);
+   }
+
+   // deQueue becomes getFirst() as Observer and deQueue() as Producer,
+   // since deQueue() now returns a new list
+   // instead of the first item in the queue.
+   public E getFirst() {
+      if (size == 0) {
+         throw new IllegalStateException("ImmutableQueue.getFirst");
+      }
+      return elements.get(0);
+   }
+
+   public ImmutableQueue<E> deQueue() {
+      if (size == 0) {
+         throw new IllegalStateException("ImmutableQueue.deQueue");
+      }
+      List<E> newElements = new ArrayList<>(elements.subList(1, size));
+      return new ImmutableQueue<>(newElements);
+   }
+
+   public boolean isEmpty() {
+      return size == 0;
+   }
+
+   public static void main(String[] args) {
+      // Simple exercise to enQueue/deQueue cmd line args
+      // Usage:  java ImmutableQueue item1 item2 item3 ...
+      ImmutableQueue<String> q = new ImmutableQueue<String>();
+      for (String arg : args) {
+         q = q.enQueue(arg);
+      }
+      while (!q.isEmpty() ) {
+         System.out.println(q.getFirst());
+         q = q.deQueue();
+      }
+   }
+}
---
+++ assignment-4/scott/src/main/java/Queue.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Queue<E>  {
+
+   private List<E> elements;
+   private int size;
+
+   public Queue() {   
+      this.elements = new ArrayList<E>();
+      this.size = 0;
+   }
+
+   public void enQueue (E e) {
+     elements.add(e);
+     size++;
+   }
+
+   // mutators
+   public E deQueue () {
+     if (size == 0) throw new IllegalStateException("Queue.deQueue");
+     E result = elements.get(0);
+     elements.remove(0);
+     size--;
+     return result;
+   }
+
+   public boolean isEmpty() {
+      return size == 0;
+   }
+
+  public static void main(String[] args) {
+     // Simple exercise to enQueue/deQueue cmd line args
+     // Usage:  java Queue item1 item2 item3 ...
+     Queue<String> q = new Queue<String>();
+     for (String arg : args)
+        q.enQueue(arg);
+     while (!q.isEmpty() )
+        System.out.println(q.deQueue());
+  }
+}
